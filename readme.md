0a73bb46a69407d480897d2be4bb7b9f1a6143db
yang <zhihong.yang@til-china.com>
2022-02-28 10:56:18 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+spring security 和 JWT
---
+++ src/main/java/com/example/testSpringSecurityWithJwt/TestSpringSecurityWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.testSpringSecurityWithJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringSecurityWithJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringSecurityWithJwtApplication.class, args);
+    }
+
+}
